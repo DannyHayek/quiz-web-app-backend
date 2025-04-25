d8dbaeb23b44f9bdfa491dcb56bea30673f9aac4
Danny <dhayek92@gmail.com>
2025-04-25 17:35:24 +0300

    Implemented functionality for register.php allowing it to create a new user on db provided that user doesn't already exist

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+
+include "./connection.php";
+
+$user = $_POST["user"];
+$pass = $_POST["pass"];
+
+try {
+
+    $sql = $connection->query("SELECT username FROM users WHERE username = $user");
+
+    $return = $sql->fetch(PDO::FETCH_ASSOC);
+    
+    if ($return){
+        echo "\n$user already exists!";
+    } else {
+        $sql = $connection->query("INSERT INTO users (username, password) VALUES ($user, $pass)");
+
+        echo "\nUser registered!";
+    }
+} catch(Exception $e) {
+    echo "Something went wrong!";
+}
+
